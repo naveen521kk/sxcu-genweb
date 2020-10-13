6b3f0218711a21f32306107b238de13a9836888e
Naveen M K <naveen@syrusdark.website>
2020-10-13 10:47:37 +0530

    production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 var webpack = require("webpack");
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'build.js',
